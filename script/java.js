73666fb55dbae3b89eb216b8ae652c7a33f8b0db
Joseph Zhang <jxsephz@gmail.com>
2019-07-22 08:35:42 -0400

    1

--- script/java.js
+++ script/java.js
@@ -35,3 +35,9 @@ function closeNav() {
 }
 
 document.addEventListener("touchstart", function(){}, true);
+
+$('#return-to-top').click(function() {      // When arrow is clicked
+  $('body,html').animate({
+      scrollTop : 0                       // Scroll to top of body
+  }, 500);
+});
