34e03f9d56c4355cff3496ad0884c94d8a79b969
Joseph Zhang <jxsephz@gmail.com>
2019-07-22 20:52:50 -0400

    1

--- script/java.js
+++ script/java.js
@@ -41,3 +41,42 @@ $('#return-to-top').click(function() {      // When arrow is clicked
       scrollTop : 0                       // Scroll to top of body
   }, 500);
 });
+
+function fadeInPage() {
+  if (!window.AnimationEvent) {
+    return;
+  }
+  var fader = document.getElementById('fader');
+  fader.classList.add('fade-out');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (!window.AnimationEvent) { return; }
+  var anchors = document.getElementsByTagName('a');
+  for (var idx=0; idx<anchors.length; idx+=1) {
+    if (anchors[idx].hostname !== window.location.hostname) {
+      continue;
+    }
+    anchors[idx].addEventListener('click', function(event) {
+      var fader = document.getElementById('fader'),
+          anchor = event.currentTarget;
+      
+      var listener = function() {
+          window.location = anchor.href;
+          fader.removeEventListener('animationend', listener);
+      };
+      fader.addEventListener('animationend', listener);
+      
+      event.preventDefault();
+      fader.classList.add('fade-in');
+    });
+}
+});
+
+window.addEventListener('pageshow', function (event) {
+  if (!event.persisted) {
+    return;
+  }
+  var fader = document.getElementById('fader');
+  fader.classList.remove('fade-in');
+});
