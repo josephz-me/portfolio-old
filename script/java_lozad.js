8794ab5f4dc43ff0477e91ab0ec30c93ca404635
Joseph Zhang <jxsephz@gmail.com>
2019-09-11 17:21:47 -0400

    1

--- script/java_lozad.js
+++ script/java_lozad.js
@@ -8,10 +8,3 @@ lozad('.lozad', {
       }
   }
 }).observe()
-
-// const observer = lozad();
-// observer.observe();
-
-// const coolImage = document.querySelector('.image-to-load-first')
-// // ... trigger the load of a image before it appears on the viewport
-// observer.triggerLoad(coolImage);
