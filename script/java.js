284186e7778cf42d68576b9b160247546a8bad7d
Joseph Zhang <jxsephz@gmail.com>
2020-03-07 11:58:50 -0800

    1

--- script/java.js
+++ script/java.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
-  $(this).scrollTop(0);
-});
+// $(document).ready(function(){
+//   $(this).scrollTop(0);
+// });
 
 // Typed.JS //
 // var typed = new Typed('#typed', {
@@ -29,41 +29,41 @@ function closeNav() {
 }
 
 // Page Transitions //
-function fadeInPage() {
-  if (!window.AnimationEvent) {
-    return;
-  }
-  var fader = document.getElementById('fader');
-  fader.classList.add('fade-out');
-}
+// function fadeInPage() {
+//   if (!window.AnimationEvent) {
+//     return;
+//   }
+//   var fader = document.getElementById('fader');
+//   fader.classList.add('fade-out');
+// }
 
-document.addEventListener('DOMContentLoaded', function() {
-  if (!window.AnimationEvent) { return; }
-  var anchors = document.getElementsByTagName('a');
-  for (var idx=0; idx<anchors.length; idx+=1) {
-    if (anchors[idx].hostname !== window.location.hostname) {
-      continue;
-    }
-    anchors[idx].addEventListener('click', function(event) {
-      var fader = document.getElementById('fader'),
-          anchor = event.currentTarget;
+// document.addEventListener('DOMContentLoaded', function() {
+//   if (!window.AnimationEvent) { return; }
+//   var anchors = document.getElementsByTagName('a');
+//   for (var idx=0; idx<anchors.length; idx+=1) {
+//     if (anchors[idx].hostname !== window.location.hostname) {
+//       continue;
+//     }
+//     anchors[idx].addEventListener('click', function(event) {
+//       var fader = document.getElementById('fader'),
+//           anchor = event.currentTarget;
       
-      var listener = function() {
-          window.location = anchor.href;
-          fader.removeEventListener('animationend', listener);
-      };
-      fader.addEventListener('animationend', listener);
+//       var listener = function() {
+//           window.location = anchor.href;
+//           fader.removeEventListener('animationend', listener);
+//       };
+//       fader.addEventListener('animationend', listener);
       
-      event.preventDefault();
-      fader.classList.add('fade-in');
-    });
-}
-});
+//       event.preventDefault();
+//       fader.classList.add('fade-in');
+//     });
+// }
+// });
 
-window.addEventListener('pageshow', function (event) {
-  if (!event.persisted) {
-    return;
-  }
-  var fader = document.getElementById('fader');
-  fader.classList.remove('fade-in');
-});
+// window.addEventListener('pageshow', function (event) {
+//   if (!event.persisted) {
+//     return;
+//   }
+//   var fader = document.getElementById('fader');
+//   fader.classList.remove('fade-in');
+// });
